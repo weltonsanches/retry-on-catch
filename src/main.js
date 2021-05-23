51c2e3eb98e47e375e3c768a4ceaed161536607d
Welton Sanches <sanches.welton@gmail.com>
2021-05-23 17:56:12 -0300

    defining params and starting execute try function

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 const sleep = (s) => new Promise((resolve) => setTimeout(resolve, (Number(s) * 1000)));
 
+const executeTry = async (callback) => {
+  return callback
+};
+
 const retry = (params, callback) => {
-  return callback;
-}
+  const {
+    tries = 3,
+    secondsBetweenTries = 3,
+    description,
+    onError,
+  } = params;
+
+  if (typeof callback !== 'function') {
+    console.error('Error when execute try, the second parameter must be a function');
+    return callback;
+  }
+
+  const firstTry = 1;
+  return executeTry(callback);
+};
 
 export default retry;
