ae9e657baf5a6ffad854d50aa40b6f5f18df1fb2
Welton Sanches <sanches.welton@gmail.com>
2021-06-26 18:04:00 -0300

    add logger config

--- src/main.js
+++ src/main.js
@@ -1,40 +1,59 @@
+/* eslint-disable no-console */
 const sleep = (s) => new Promise((resolve) => setTimeout(resolve, (Number(s) * 1000)));
 
-const executeTry = async (callback, time, maxTimes, secondsBetweenTries, description, onError) => {
+const executeTry = async ({
+  secondsBetweenTries,
+  description,
+  callback,
+  maxTimes,
+  onError,
+  logInfo,
+  time,
+}) => {
   const message = description ? `| ${description}` : '';
 
   try {
     return callback();
   } catch (error) {
     if (time >= maxTimes) {
-      console.log(`Max tries limit reached ${message}`);
-      console.log('Error on the last try:', error);
+      logInfo(`Max tries limit reached ${message}`);
+      logInfo('Error on the last try:', error);
       if (onError && typeof onError !== 'function') {
         onError(error);
       }
       return error;
     }
     await sleep(secondsBetweenTries);
-    console.log(`Executing trie ${time} ${message}`);
+    logInfo(`Executing trie ${time} ${message}`);
     return executeTry(callback, time + 1, maxTimes, secondsBetweenTries, description, onError);
   }
 };
 
 const retry = (params, callback) => {
   const {
-    tries = 3,
     secondsBetweenTries = 3,
+    logInfo = console.log,
+    logError = console.error,
+    tries = 3,
     description,
     onError,
   } = params;
 
   if (typeof callback !== 'function') {
-    console.error('Error when execute try, the second parameter must be a function');
+    logError('Error when execute try, the second parameter must be a function');
     return callback;
   }
 
   const firstTry = 1;
-  return executeTry(callback, firstTry, tries, secondsBetweenTries, description, onError);
+  return executeTry({
+    maxTimes: tries,
+    time: firstTry,
+    secondsBetweenTries,
+    description,
+    callback,
+    logInfo,
+    onError,
+  });
 };
 
 export default retry;
