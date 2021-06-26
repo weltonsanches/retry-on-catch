f9ef70a1541e73e851983d175215699d7f58cda8
Welton Sanches <sanches.welton@gmail.com>
2021-06-26 18:35:44 -0300

    separate execute try in another file

---
+++ src/execution.js
@@ -0,0 +1,31 @@
+const { sleep } = require('./utils');
+
+const executeTry = async ({
+  secondsBetweenTries,
+  description,
+  callback,
+  maxTimes,
+  onError,
+  logInfo,
+  time,
+}) => {
+  const message = description ? `| ${description}` : '';
+
+  try {
+    return callback();
+  } catch (error) {
+    if (time >= maxTimes) {
+      logInfo(`Max tries limit reached ${message}`);
+      logInfo('Error on the last try:', error);
+      if (onError && typeof onError !== 'function') {
+        onError(error);
+      }
+      return error;
+    }
+    await sleep(secondsBetweenTries);
+    logInfo(`Executing trie ${time} ${message}`);
+    return executeTry(callback, time + 1, maxTimes, secondsBetweenTries, description, onError);
+  }
+};
+
+module.exports = executeTry;
--- src/main.js
+++ src/main.js
@@ -1,33 +1,5 @@
 /* eslint-disable no-console */
-const sleep = (s) => new Promise((resolve) => setTimeout(resolve, (Number(s) * 1000)));
-
-const executeTry = async ({
-  secondsBetweenTries,
-  description,
-  callback,
-  maxTimes,
-  onError,
-  logInfo,
-  time,
-}) => {
-  const message = description ? `| ${description}` : '';
-
-  try {
-    return callback();
-  } catch (error) {
-    if (time >= maxTimes) {
-      logInfo(`Max tries limit reached ${message}`);
-      logInfo('Error on the last try:', error);
-      if (onError && typeof onError !== 'function') {
-        onError(error);
-      }
-      return error;
-    }
-    await sleep(secondsBetweenTries);
-    logInfo(`Executing trie ${time} ${message}`);
-    return executeTry(callback, time + 1, maxTimes, secondsBetweenTries, description, onError);
-  }
-};
+const executeTry = require('./execution');
 
 const retry = (params, callback) => {
   const {
